3de96f93382b5ed969e05646cbc4c9cc5304d6ea
EddieBWXiao <xiao.bowen.eddie@gmail.com>
2025-02-20 10:53:23 +0000

    fixed bug: forgot to pass prc_config; add sampleModel
    the sampleModel approach: also good recovery, but still with bias against high learning rates

--- HGF/test_param_recov/test1_rw_param_recov_sampleModel.m
+++ HGF/test_param_recov/test1_rw_param_recov_sampleModel.m
@@ -1,20 +1,17 @@
 addpath(genpath('../'))
 
+% simplify recovery pipeline by using tapas_sampleModel
+% check issue with high learning rates (high al) -- failure to recover??
+
 u = load('example_binary_input.txt');
 
 %% settings
-nsims = 100;
+nsims = 1000;
 %u = [u;u;u;u;u];
 
 %% configure the priors
-
 prc_config = tapas_rw_binary_config();
 obs_config = tapas_unitsq_sgm_config();
-
-% the following reached 0.86 & 0.91 in recovery, but still a 0.66 plateau for LR:
-% prc_config.logitalsa = 0.5;
-% obs_config.logzesa = 3;
-% obs_config.logzemu = 1;
 
 % priors:
 prc_config.logitalmu = 0;
@@ -28,19 +25,9 @@ obs_config = tapas_align_priors(obs_config);
 
 % configure the fitting
 optim_config = tapas_quasinewton_optim_config();
-optim_config.nRandInit = 5; %multiple initializations for optimization?
+optim_config.nRandInit = 0; %multiple initializations for optimization?
 optim_config.maxIter = 10000; % iteration, increased from 100;
 optim_config.maxRst = 100; %max reset, increased from 10;
-
-%% set parameters for simulations
-logisticf = @(x) 1./(1+exp(-x));
-%al_range = logisticf(normrnd(0,1,[nsims,1]));
-al_range = unifrnd(0,1,[nsims,1]);
-zeta_range = normrnd(obs_config.priormus(1),obs_config.priorsas(1),[nsims,1]);
-while any(zeta_range<0)
-    ind_replace = zeta_range<0;
-    zeta_range(ind_replace) = normrnd(obs_config.priormus(1),obs_config.priorsas(1),[sum(ind_replace),1]);
-end
 
 %% loop for recovery
 % preallocate
@@ -48,11 +35,9 @@ sims = cell(nsims,1);
 fitted = cell(nsims,1);
 % use for loop
 for i = 1:nsims
-    sims{i} = tapas_simModel(u,...
-        'tapas_rw_binary',...
-        [0.5, al_range(i)],... 
-        'tapas_unitsq_sgm',...  - note that this will run _sim which doesn't transform the value
-        zeta_range(i));
+    sims{i} = tapas_sampleModel(u,...
+        prc_config,...
+        obs_config);
     
     fitted{i} = tapas_fitModel(sims{i}.y,...
         u,...
